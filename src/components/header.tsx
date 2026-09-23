cf240e90199966ec3ef7ad98f7d47d0216356e07
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-09-23 19:26:49 +0530

    search added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Input,
 } from "@nextui-org/react";
 import HeaderAuth from "@/components/header-auth";
+import SearchInput from "@/components/search-input";
 
 export default function Header() {
   return (
@@ -19,7 +20,9 @@ export default function Header() {
 
       <NavbarContent justify="center">
         <NavbarItem>
-          <Input />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
 
