7d2c7a8e5e2c573aaa58923b8fcc80c0b7a97c31
rahulfrontend1-cmd <rahul.frontend1@gmail.com>
2026-09-23 08:02:30 +0530

    pages added

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from "@/components/post/post-create-form";
+import PostList from "@/components/posts/post-list";
+import { fetchPostsByTopicSlug } from "@/db/queries/posts";
 
 interface TopicShowPageProps {
   params: Promise<{
@@ -7,14 +9,17 @@ interface TopicShowPageProps {
 }
 
 export default async function TopicShowPage({ params }: TopicShowPageProps) {
-    const { slug } = await params;
+  const { slug } = await params;
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
       </div>
 
-      <div><PostCreateForm slug={slug} /></div>
+      <div>
+        <PostCreateForm slug={slug} />
+      </div>
     </div>
   );
 }
---
+++ src/db/queries/posts.ts
@@ -0,0 +1,42 @@
+import type { Post } from "@prisma/client";
+import { db } from "@/db";
+
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+
+// export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number] 
+// here number indicates take the type of 1 of the data in lamen term it's [0]
+// Then Remove return type, 'type' from fetchPostsByTopicSlug i.e : Promise<PostWithData[]>
+
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    where: { topic: { slug } },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+  return db.post.findMany({
+    orderBy: [
+      {
+        comments: {
+          _count: "desc",
+        },
+      },
+    ],
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+    take: 5,
+  });
+}
+
